b4a0fbbb43073db7b15b64b8124807b3a23259cc
Arthur Borges <arthurborges.dev@gmail.com>
2020-04-18 17:01:43 -0300

    The bot can read the last 2 messages of channel history

--- demogorgon.rb
+++ demogorgon.rb
@@ -1,14 +1,17 @@
-require 'discordrb'
+require "discordrb"
 
-bot = Discordrb::Commands::CommandBot.new token: ENV['DEMOGORGON_TOKEN'], prefix: '/demo '
+bot = Discordrb::Commands::CommandBot.new token: ENV["DEMOGORGON_TOKEN"], prefix: "/demo "
+
+lastDown = 0
 
 bot.command :down do |event, number|
-    #event << "dices are down by " + number
-    event << event.user 
+    event << "dices are down by " + number
+    event.channel.history(2).each { |msg| event << msg }
+    return nil
 end
 
 bot.command(:rand, description: 'Generates a random number 1 and max') do |_event, max|
-    "dices are down by " + rand(1..max.to_i).to_s
+    "dices are down by " + rand(1...max.to_i).to_s
 end
 
 bot.run
